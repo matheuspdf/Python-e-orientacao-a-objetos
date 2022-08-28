cc675643f6df12b95584d960556c257ce0add8ac
matheuspdf <96511087+matheuspdf@users.noreply.github.com>
2022-08-28 20:46:25 -0300

    script.py

---
+++ 5_String em Python extraindo informações de uma URL/main.py
@@ -0,0 +1,16 @@
+url = "bytebank.com/cambio?moedaOrigem=real"
+print(url)
+
+url_base = url[0:19]
+print(url_base)  # Vai imprimir “bytebank.com/cambio”
+
+url_parametros = url[20:36]
+print(url_parametros)
+
+
+nome_completo = "Matheus Lopes"
+
+primeiro_nome = nome_completo[0:7]
+segundo_nome = nome_completo[8:13]
+
+print(primeiro_nome, segundo_nome)
