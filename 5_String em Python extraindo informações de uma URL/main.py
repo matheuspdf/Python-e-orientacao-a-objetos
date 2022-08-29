e54ffd5a09227bc9aa7dda1a0e31d745149c0b25
matheuspdf <96511087+matheuspdf@users.noreply.github.com>
2022-08-29 20:59:31 -0300

    script.py

--- 5_String em Python extraindo informações de uma URL/main.py
+++ 5_String em Python extraindo informações de uma URL/main.py
@@ -1,16 +1,15 @@
-url = "bytebank.com/cambio?moedaOrigem=real"
-print(url)
+url = "bytebank.com/cambio?quantidade=100&moedaOrigem=real&moedaDestino=dolar"
+indice_interrogacao = url.find('?')
+url_base = url[:indice_interrogacao]
+url_parametros = url[indice_interrogacao+1:]
 
-url_base = url[0:19]
-print(url_base)  # Vai imprimir “bytebank.com/cambio”
+parametro_busca = 'quantidade'
+indice_parametro = url_parametros.find(parametro_busca)
+indice_valor = indice_parametro + len(parametro_busca) + 1
 
-url_parametros = url[20:36]
-print(url_parametros)
-
-
-nome_completo = "Matheus Lopes"
-
-primeiro_nome = nome_completo[0:7]
-segundo_nome = nome_completo[8:13]
-
-print(primeiro_nome, segundo_nome)
+indice_e_comercial = url_parametros.find('&', indice_valor)
+if indice_e_comercial == -1:
+    valor = url_parametros[indice_valor:]
+else:
+    valor = url_parametros[indice_valor:indice_e_comercial]
+print(valor)
