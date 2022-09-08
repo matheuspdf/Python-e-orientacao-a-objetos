1c3cfe2272b6b1ce4ec046894a071aef0a4cfbc3
matheuspdf <96511087+matheuspdf@users.noreply.github.com>
2022-09-08 18:48:15 -0300

    script.py

--- 6_Python Collections parte 1 listas e tuplas/introdução_a_collections.py
+++ 6_Python Collections parte 1 listas e tuplas/introdução_a_collections.py
@@ -339,3 +339,377 @@ numeros
 
 numeros + 3
 
+class ContaSalario:
+  
+  def __init__(self, codigo):
+    self._codigo = codigo
+    self._saldo = 0
+  
+  def deposita(self, valor):
+    self._saldo += valor
+    
+  def __str__(self):
+    return "[>>Codigo {} Saldo {}<<]".format(self._codigo, self._saldo)
+
+conta1 = ContaSalario(37)
+print(conta1)
+
+conta2 = ContaSalario(37)
+print(conta2)
+
+conta1 == conta2
+
+contas = [conta1]
+conta1 in contas
+
+conta2 in contas
+
+class ContaSalario:
+  
+  def __init__(self, codigo):
+    self._codigo = codigo
+    self._saldo = 0
+    
+  def __eq__(self, outro):
+    return self._codigo == outro._codigo
+  
+  def deposita(self, valor):
+    self._saldo += valor
+    
+  def __str__(self):
+    return "[>>Codigo {} Saldo {}<<]".format(self._codigo, self._saldo)
+
+conta1 = ContaSalario(37)
+conta2 = ContaSalario(37)
+conta1 == conta2
+
+conta1 != conta2
+
+conta1 in [conta2]
+
+conta2 in [conta1]
+
+class ContaSalario:
+  
+  def __init__(self, codigo):
+    self._codigo = codigo
+    self._saldo = 0
+    
+  def __eq__(self, outro):
+    return self._codigo == outro._codigo and self._saldo == outro._saldo
+  
+  def deposita(self, valor):
+    self._saldo += valor
+    
+  def __str__(self):
+    return "[>>Codigo {} Saldo {}<<]".format(self._codigo, self._saldo)
+
+conta1 = ContaSalario(37)
+conta2 = ContaSalario(37)
+conta1 == conta2
+
+conta1.deposita(10)
+
+conta1 == conta2
+
+class ContaSalario:
+  
+  def __init__(self, codigo):
+    self._codigo = codigo
+    self._saldo = 0
+    
+  def __eq__(self, outro):
+    if type(outro) != ContaSalario:
+      return False
+    
+    return self._codigo == outro._codigo and self._saldo == outro._saldo
+  
+  def deposita(self, valor):
+    self._saldo += valor
+    
+  def __str__(self):
+    return "[>>Codigo {} Saldo {}<<]".format(self._codigo, self._saldo)
+
+conta1 = ContaSalario(37)
+conta2 = ContaCorrente(37)
+
+conta1 == conta2
+
+isinstance(ContaCorrente(34), ContaCorrente)
+
+isinstance(ContaCorrente(34), Conta)
+
+idades = [15, 87, 32, 65, 56, 32, 49, 37]
+
+for i in range(len(idades)):
+  print(i, idades[i])
+
+range(len(idades)) # lazy...
+
+enumerate(idades) # lazy
+
+list(range(len(idades))) # forcei a geração dos valores
+
+list(enumerate(idades))
+
+for indice, idade in enumerate(idades): # unpacking da nossa tupla
+  print(indice, "x", idade)
+
+usuarios = [
+    ("Guilherme", 37, 1981),
+    ("Daniela", 31, 1987),
+    ("Paulo", 39, 1979)
+]
+
+for nome, idade, nascimento in usuarios: # ja desempacotando
+  print(nome)
+
+for nome, _, _ in usuarios: # ja desempacotando, ignorando o resto
+  print(nome)
+
+idades
+
+sorted(idades)
+
+15 < 32
+
+list(reversed(idades))
+
+sorted(idades, reverse=True)
+
+list(reversed(sorted(idades)))
+
+idades
+
+idades.sort()
+
+idades
+
+15 < 32
+
+nomes = ["guilherme", "Daniela", "Paulo"]
+sorted(nomes)
+
+class ContaSalario:
+  
+  def __init__(self, codigo):
+    self._codigo = codigo
+    self._saldo = 0
+    
+  def __eq__(self, outro):
+    if type(outro) != ContaSalario:
+      return False
+    
+    return self._codigo == outro._codigo and self._saldo == outro._saldo
+  
+  def deposita(self, valor):
+    self._saldo += valor
+    
+  def __str__(self):
+    return "[>>Codigo {} Saldo {}<<]".format(self._codigo, self._saldo)
+
+conta_do_guilherme = ContaSalario(17)
+conta_do_guilherme.deposita(500)
+
+conta_da_daniela = ContaSalario(3)
+conta_da_daniela.deposita(1000)
+
+conta_do_paulo = ContaSalario(133)
+conta_do_paulo.deposita(510)
+
+contas = [conta_do_guilherme, conta_da_daniela, conta_do_paulo]
+
+for conta in contas:
+  print(conta)
+
+sorted(contas)
+
+conta_do_guilherme < conta_da_daniela
+
+def extrai_saldo(conta):
+  return conta._saldo
+
+for conta in sorted(contas, key=extrai_saldo):
+  print(conta)
+
+from operator import attrgetter
+
+for conta in sorted(contas, key=attrgetter("_saldo")):
+  print(conta)
+
+conta_do_guilherme < conta_da_daniela
+
+class ContaSalario:
+  
+  def __init__(self, codigo):
+    self._codigo = codigo
+    self._saldo = 0
+    
+  def __eq__(self, outro):
+    if type(outro) != ContaSalario:
+      return False
+    
+    return self._codigo == outro._codigo and self._saldo == outro._saldo
+  
+  def __lt__(self, outro):
+    return self._saldo < outro._saldo
+  
+  def deposita(self, valor):
+    self._saldo += valor
+    
+  def __str__(self):
+    return "[>>Codigo {} Saldo {}<<]".format(self._codigo, self._saldo)
+
+conta_do_guilherme = ContaSalario(17)
+conta_do_guilherme.deposita(500)
+
+conta_da_daniela = ContaSalario(3)
+conta_da_daniela.deposita(1000)
+
+conta_do_paulo = ContaSalario(133)
+conta_do_paulo.deposita(510)
+
+contas = [conta_do_guilherme, conta_da_daniela, conta_do_paulo]
+
+conta_do_guilherme < conta_da_daniela
+
+conta_do_guilherme > conta_da_daniela
+
+for conta in sorted(contas):
+  print(conta)
+
+for conta in sorted(contas,reverse=True):
+  print(conta)
+
+conta_do_guilherme = ContaSalario(1700)
+conta_do_guilherme.deposita(500)
+
+conta_da_daniela = ContaSalario(3)
+conta_da_daniela.deposita(1000)
+
+conta_do_paulo = ContaSalario(133)
+conta_do_paulo.deposita(500)
+
+contas = [conta_do_guilherme, conta_da_daniela, conta_do_paulo]
+
+for conta in sorted(contas, key=attrgetter("_saldo", "_codigo")):
+  print(conta)
+
+conta_do_guilherme = ContaSalario(1700)
+conta_do_guilherme.deposita(500)
+
+conta_da_daniela = ContaSalario(3)
+conta_da_daniela.deposita(500)
+
+conta_do_paulo = ContaSalario(133)
+conta_do_paulo.deposita(500)
+
+contas = [conta_do_guilherme, conta_da_daniela, conta_do_paulo]
+
+for conta in sorted(contas, key=attrgetter("_saldo", "_codigo")):
+  print(conta)
+
+class ContaSalario:
+  
+  def __init__(self, codigo):
+    self._codigo = codigo
+    self._saldo = 0
+    
+  def __eq__(self, outro):
+    if type(outro) != ContaSalario:
+      return False
+    
+    return self._codigo == outro._codigo and self._saldo == outro._saldo
+  
+  def __lt__(self, outro):
+    if self._saldo != outro._saldo:
+      return self._saldo < outro._saldo
+    
+    return self._codigo < outro._codigo
+  
+  def deposita(self, valor):
+    self._saldo += valor
+    
+  def __str__(self):
+    return "[>>Codigo {} Saldo {}<<]".format(self._codigo, self._saldo)
+
+conta_do_guilherme = ContaSalario(1700)
+conta_do_guilherme.deposita(500)
+
+conta_da_daniela = ContaSalario(3)
+conta_da_daniela.deposita(500)
+
+conta_do_paulo = ContaSalario(133)
+conta_do_paulo.deposita(500)
+
+contas = [conta_do_guilherme, conta_da_daniela, conta_do_paulo]
+
+for conta in sorted(contas):
+  print(conta)
+
+conta_do_guilherme = ContaSalario(1700)
+conta_do_guilherme.deposita(500)
+
+conta_da_daniela = ContaSalario(3)
+conta_da_daniela.deposita(1000)
+
+conta_do_paulo = ContaSalario(133)
+conta_do_paulo.deposita(500)
+
+contas = [conta_do_guilherme, conta_da_daniela, conta_do_paulo]
+
+for conta in sorted(contas):
+  print(conta)
+
+conta_do_guilherme < conta_da_daniela
+
+conta_do_guilherme <= conta_da_daniela
+
+from functools import total_ordering
+
+@total_ordering
+class ContaSalario:
+  
+  def __init__(self, codigo):
+    self._codigo = codigo
+    self._saldo = 0
+    
+  def __eq__(self, outro):
+    if type(outro) != ContaSalario:
+      return False
+    
+    return self._codigo == outro._codigo and self._saldo == outro._saldo
+  
+  def __lt__(self, outro):
+    if self._saldo != outro._saldo:
+      return self._saldo < outro._saldo
+    
+    return self._codigo < outro._codigo
+  
+  def deposita(self, valor):
+    self._saldo += valor
+    
+  def __str__(self):
+    return "[>>Codigo {} Saldo {}<<]".format(self._codigo, self._saldo)
+
+conta_do_guilherme = ContaSalario(1700)
+conta_do_guilherme.deposita(500)
+
+conta_da_daniela = ContaSalario(3)
+conta_da_daniela.deposita(1000)
+
+conta_do_paulo = ContaSalario(133)
+conta_do_paulo.deposita(500)
+
+contas = [conta_do_guilherme, conta_da_daniela, conta_do_paulo]
+
+conta_do_guilherme <= conta_da_daniela
+
+conta_do_guilherme <= conta_do_paulo
+
+conta_do_guilherme < conta_do_guilherme
+
+conta_do_guilherme == conta_do_guilherme
+
+conta_do_guilherme <= conta_do_guilherme
+
